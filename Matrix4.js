13c091411341c8d1d7df243c3582d7c37956edbf
Ander <anderpang@qq.com>
2019-05-24 12:03:07 +0800

    Update Matrix4.js

--- Matrix4.js
+++ Matrix4.js
@@ -170,39 +170,43 @@
         },
         invert:function(){
           var m=this.data,
-              a=m.slice();
-          var a00 = a[0],
+              a=m.slice(),
+              
+              a00 = a[0],
               a01 = a[1],
               a02 = a[2],
-              a03 = a[3];
-          var a10 = a[4],
+              a03 = a[3],
+              
+              a10 = a[4],
               a11 = a[5],
               a12 = a[6],
-              a13 = a[7];
-          var a20 = a[8],
+              a13 = a[7],
+              
+              a20 = a[8],
               a21 = a[9],
               a22 = a[10],
-              a23 = a[11];
-          var a30 = a[12],
+              a23 = a[11],
+
+              a30 = a[12],
               a31 = a[13],
               a32 = a[14],
-              a33 = a[15];
+              a33 = a[15],
 
-          var b00 = a00 * a11 - a01 * a10;
-          var b01 = a00 * a12 - a02 * a10;
-          var b02 = a00 * a13 - a03 * a10;
-          var b03 = a01 * a12 - a02 * a11;
-          var b04 = a01 * a13 - a03 * a11;
-          var b05 = a02 * a13 - a03 * a12;
-          var b06 = a20 * a31 - a21 * a30;
-          var b07 = a20 * a32 - a22 * a30;
-          var b08 = a20 * a33 - a23 * a30;
-          var b09 = a21 * a32 - a22 * a31;
-          var b10 = a21 * a33 - a23 * a31;
-          var b11 = a22 * a33 - a23 * a32;
+              b00 = a00 * a11 - a01 * a10,
+              b01 = a00 * a12 - a02 * a10,
+              b02 = a00 * a13 - a03 * a10,
+              b03 = a01 * a12 - a02 * a11,
+              b04 = a01 * a13 - a03 * a11,
+              b05 = a02 * a13 - a03 * a12,
+              b06 = a20 * a31 - a21 * a30,
+              b07 = a20 * a32 - a22 * a30,
+              b08 = a20 * a33 - a23 * a30,
+              b09 = a21 * a32 - a22 * a31,
+              b10 = a21 * a33 - a23 * a31,
+              b11 = a22 * a33 - a23 * a32,
 
           // Calculate the determinant
-          var det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+              det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
 
           if (!det) {
             return null;
